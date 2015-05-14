ef9b1397b103065da777de1278d8c280570de104
Julio Lopez <ljuliom@gmail.com>
2015-05-13 20:16:59 -0500

    hello world from the framework

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,5 +1,9 @@
-require "./version"
+require "version"
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type'=> 'text/html'}, ['Hello World']]
+    end
+  end
 end
