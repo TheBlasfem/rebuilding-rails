0a51c208323c1cf82c2e525f9c604439b9f728ac
Julio Lopez <ljuliom@gmail.com>
2015-06-23 23:53:23 -0500

    added controllers

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,9 +1,27 @@
-require "version"
+require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [200, {'Content-Type'=> 'text/html'}, ['Hello World']]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {'Content-Type'=> 'text/html'}, []]
+      end
+
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+      [200, {'Content-Type'=> 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
